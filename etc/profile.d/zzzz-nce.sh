ee83bdec3ffcf97506f6002977c47d089e65543e
Tom Hornos <tom.hornos@gmail.com>
2013-05-31 15:57:42 +0200

    fix

--- etc/profile.d/zzzz-nce.sh
+++ etc/profile.d/zzzz-nce.sh
@@ -2,6 +2,9 @@
 if ! test -r "/etc/NCE_SYSTEM" ; then
   echo "Set /etc/NCE_SYSTEM"
 else
+	# clean
+	module unuse /usr/share/modules/modulefiles
+  module purge
   module use /opt/nce/modulefiles
   module load nce/global
   module load $(cat /etc/NCE_SYSTEM)
@@ -9,9 +12,9 @@ else
 fi
 
 if ! test -z "${NCE_SCHEDULER}" ; then
-	module load ${NCE_SCHEDULER}
+  module load ${NCE_SCHEDULER}
 fi
 
 if ! test -z "${NCE_DEFAULT_MPI}" ; then
-	module load ${NCE_DEFAULT_MPI}
+  module load ${NCE_DEFAULT_MPI}
 fi
